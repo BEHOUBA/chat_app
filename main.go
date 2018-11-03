bf7f5c04ed7ca8bb56042629af8e0f2c353a7624
behouba <behouba@gmail.com>
2018-11-03 16:56:17 +0300

    .gitignore updated

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/astaxie/beego"
 	_ "github.com/behouba/chat_app/models"
 	_ "github.com/behouba/chat_app/routers"
 )
 
 func main() {
-	beego.Run()
+	// port, err := strconv.Atoi(os.Getenv("PORT"))
+	// if err == nil {
+	// 	beego.HttpPort = port
+	// }
+	beego.Run(":" + os.Getenv("PORT"))
 }
